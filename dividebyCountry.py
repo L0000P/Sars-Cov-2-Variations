f4f78d1b37a7e62f51bf4f4b81d009b22ce8e828
Alessandro Tomarchio <alessandro.tomarchio01@gmail.com>
2024-03-14 08:42:56 +0100

    Refactor dividebyCountry.py to improve code readability and performance

--- dividebyCountry.py
+++ dividebyCountry.py
@@ -1,11 +1,26 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, regexp_replace
+from pyspark.sql.functions import expr, col, array, concat_ws
 import os
 import logging
 import shutil
 import urllib.parse
-import pandas as pd
 import re
+import subprocess
+
+def process_dataframe(file_path, diff_temp_dir):
+    print(f"Processing file {file_path}")
+    df = spark.read.format('csv').option('header', 'true').option('delimiter', '\t').option('inferSchema', 'true').load(file_path)
+    df = df.toDF(*(c.replace('.', '_') for c in df.columns))
+    
+    clade_index = df.columns.index("Country") + 6 if "sample" in df.columns else df.columns.index("Country") + 5
+    mutation_columns = df.columns[clade_index + 1:]
+
+    df = df.withColumn("Mutations", array([expr(f"CASE WHEN `{col}` = 1 THEN '{col}' END") for col in mutation_columns]))
+
+    df_final = df.select("Country", "Geo_Location", col(df.columns[clade_index]).alias("Clade"), "Mutations")
+    df_final = df_final.withColumn("Mutations", concat_ws(",", "Mutations"))
+    df_final.repartition("Country").write.partitionBy("Country").option("header", "true").format("csv").mode("overwrite").save(diff_temp_dir)
+    df.unpersist()
 
 def safe_mkdirs(path):
     try:
@@ -17,16 +32,22 @@ def safe_mkdirs(path):
 def rename_partitions(src_dir, dst_dir):
     for subdir in os.listdir(src_dir):
         if subdir.startswith('Country='):
-            country = urllib.parse.unquote(subdir.replace('Country=', ''))
-            # Use regular expression to clean special characters
-            # This regex replaces anything that is not an alphanumeric character or an underscore with an underscore
-            country = re.sub(r'[^\w\s]', '_', country)
-            # Replace spaces with underscores for consistency
-            country = country.replace(' ', '_')
-            country_path = os.path.join(dst_dir, country)
+            # Decodifica URL per gestire correttamente i caratteri speciali come %3A
+            country_encoded = subdir.replace('Country=', '')
+            country_decoded = urllib.parse.unquote(country_encoded)
+            
+            # Sostituisci la virgola e altri caratteri non desiderati con underscore
+            # e rimuovi gli spazi extra
+            country_sanitized = re.sub(r'[^\w\s]', '_', country_decoded)
+            country_sanitized = re.sub(r'\s+', '_', country_sanitized)
+            
+            country_path = os.path.join(dst_dir, country_sanitized)
             safe_mkdirs(country_path)
+            
             for file in os.listdir(os.path.join(src_dir, subdir)):
-                shutil.move(os.path.join(src_dir, subdir, file), os.path.join(country_path, file))
+                src_file_path = os.path.join(src_dir, subdir, file)
+                dst_file_path = os.path.join(country_path, file)
+                shutil.move(src_file_path, dst_file_path)
 
 def cleanup_temp_dir(temp_dir):
     if os.path.exists(temp_dir):
@@ -36,27 +57,20 @@ def cleanup_temp_dir(temp_dir):
 def merge_and_clean(cartella_output):
     for country_dir in os.listdir(cartella_output):
         print(f"Merge {country_dir}...")
-        merged_df = pd.DataFrame()
         full_path = os.path.join(cartella_output, country_dir)
         if os.path.isdir(full_path):
-            all_files = [os.path.join(full_path, f) for f in os.listdir(full_path) if os.path.isfile(os.path.join(full_path, f)) and f.endswith('.csv')]
-            if all_files:
-                for file in all_files:
-                    df = pd.read_csv(file, low_memory=False)
-                    merged_df = pd.concat([merged_df, df], ignore_index=True)
-                    os.remove(file)
-                merged_file_path = os.path.join(cartella_output, f"{country_dir}.csv")
-                merged_df.to_csv(merged_file_path, index=False)
-                shutil.rmtree(full_path)
-
+            cmd_merge = f'copy /b "{full_path}\\part*.csv" "{cartella_output}\\{country_dir}.csv"'
+            subprocess.call(cmd_merge, shell=True)
+            cmd_del = f'del "{full_path}\\part*.csv"'
+            subprocess.call(cmd_del, shell=True)
+            shutil.rmtree(full_path)
 
 cartella_input = "matrici_ncbi_2021_2022"
 cartella_output = "Countries"
 temp_dir = "temp"
 
-os.makedirs(cartella_output, exist_ok=True)
-
-logging.getLogger("py4j").setLevel(logging.ERROR)
+if os.path.exists(temp_dir):
+    shutil.rmtree(temp_dir)
 
 spark = SparkSession.builder \
     .appName("dividebyCountry") \
@@ -67,24 +81,22 @@ spark = SparkSession.builder \
     .config("spark.sql.shuffle.partitions", "300") \
     .config("spark.sql.debug.maxToStringFields", 200) \
     .config("spark.broadcast.blockSize", "10m") \
+    .config("spark.local.dir", temp_dir) \
     .getOrCreate()
 
-for file_name in os.listdir(cartella_input):
-    file_path = os.path.join(cartella_input, file_name)
-    diff_temp_dir = os.path.join(temp_dir, file_name)  # Use file_name to create a unique temp directory for each file
-    
-    if os.path.exists(diff_temp_dir):
-        print(f"Directory {diff_temp_dir} already exists. Skipping file {file_name}.")
-        continue 
-    
-    print(f"Processing file {file_path}")
-    df = spark.read.format('csv').option('header', 'true').option('delimiter', '\t').option('inferSchema', 'true').load(file_path)
-    df = df.withColumn("Country", regexp_replace(col("Country"), "\\.", "_"))
-    df.repartition("Country").write.partitionBy("Country").option("header", "true").mode("append").format("csv").save(diff_temp_dir)
-    df.unpersist()
-    rename_partitions(diff_temp_dir, cartella_output)
-    cleanup_temp_dir(diff_temp_dir)  # Cleanup after the operation
+os.makedirs(cartella_output, exist_ok=True)
 
+
+for file_name in os.listdir(cartella_input):
+    spark.catalog.clearCache()
+    
+    file_path = os.path.join(cartella_input, file_name)
+    diff_temp_dir = os.path.join(temp_dir, file_name)
+
+    process_dataframe(file_path, diff_temp_dir)
+    rename_partitions(diff_temp_dir, cartella_output)
+
+cleanup_temp_dir(temp_dir)
 # After processing all files, merge CSV files in each country directory
 merge_and_clean(cartella_output)
 
